6a6a1ae90fe7c09b493351427ea8395d92130cdc
Fernanda Sayuri Oshiro <fsayuri-@c3r9p4.42sp.org.br>
2026-05-15 14:31:23 -0300

    initial commit

---
+++ Makefile
@@ -0,0 +1,22 @@
+NAME = libft.a
+CFLAGS = -Wall -Wextra -Werror
+SRCS = ft_isalpha.c ft_isdigit.c ft_isalnum.c ft_isascii.c ft_isprint.c
+OBJS = $(SRCS:.c=.o)
+
+all: $(NAME)
+
+$(NAME): $(OBJS)
+	ar rcs $(NAME) $(OBJS)
+
+%.o: %.c
+	$(CC) $(CFLAGS) -c $< -o $@
+
+clean:
+	rm -f $(OBJS)
+
+fclean: clean
+	rm -f $(NAME)
+
+re: fclean all
+
+.PHONY: all clean fclean re
---
+++ libft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fsayuri- <fsayuri-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/15 13:17:56 by fsayuri-          #+#    #+#             */
+/*   Updated: 2026/05/15 14:28:41 by fsayuri-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+int	ft_isalpha(char c);
+int	ft_isdigit(char c);
+int	ft_isalnum(char c);
+int	ft_isascii(char c);
+int	ft_isprint(char c);
+
+#endif
